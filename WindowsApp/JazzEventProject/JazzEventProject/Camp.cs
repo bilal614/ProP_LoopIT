7ef1a18fecf3a6d963330c4bb4005c9961e75f00
BilalTButt <bilalbutt.614@gmail.com>
2015-11-08 14:15:19 +0100

    Camp classes added
    Added the camp reservation class and camp class.

---
+++ WindowsApp/JazzEventProject/JazzEventProject/Camp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JazzEventProject_V1
+{
+    class Camp
+    {
+        //instance variables
+        private int campId;
+        private int maxPerson;
+
+        //properties
+        public int CampId
+        { get { return campId; } }
+
+        public string Description { get; set; }
+
+        public int MaxPerson
+        {
+            get { return maxPerson; }
+            set
+            {
+                if (value > 0 && value <= 6)//camp size cannot take more than 6 people but must be more than 0
+                    maxPerson = value;
+            }
+        }
+
+        public bool Available { get; set; }
+        //constructors
+        public Camp(int id, string desc, int size, bool available)
+        {
+            this.campId = id;
+            this.Description = desc;
+            this.MaxPerson = size;
+            this.Available = available;
+        }
+        //methods
+
+    }
+}
---
+++ WindowsApp/JazzEventProject/JazzEventProject/CampRes.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JazzEventProject_V1
+{
+    class CampRes
+    {
+        //instance variables
+        private int campResNo;
+        private int campId;
+        /*should AccountId be taken from parent class EventAccount to establish the link between the classes??
+         Or should it be a separate class on its own, then how will you make the link?*/
+        private DateTime startDate;
+        private DateTime endDate;
+
+        //properties
+        public int CampResNo//defines a unique id for the reservation made by customers, used as primary key for entity
+        { get { return campResNo; } }//read-only
+
+        public int CampId//used to describe unique location of camp on the map of the camp
+        { get { return campId; } }//read-only
+
+        public DateTime StartDate
+        { 
+            get { return startDate; }
+
+            set 
+            {
+                if (value < EndDate)
+                { startDate = value; }
+            }//can add restrictions to StartDate depending on circumstances such as dates of event etc.
+        }
+
+        public DateTime EndDate
+        {
+            get { return endDate; }
+            set 
+            {
+                if (value > StartDate)
+                { endDate = value; }
+            }
+        }
+        //constructors
+        public CampRes(int campResNo, int campID,/*int accountID,*/DateTime startDate, DateTime endDate)
+        {
+            this.campResNo = campResNo;
+            this.campId = campID;
+            //will the AccountId come from parent class EventAccount??
+            this.StartDate = startDate;
+            this.EndDate = endDate;
+        }
+        //methods
+
+
+    }
+}
