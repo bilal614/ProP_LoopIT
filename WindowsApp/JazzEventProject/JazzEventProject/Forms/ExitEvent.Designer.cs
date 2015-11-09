3e5de21e68058bdafc567f726c10d428fed67e5a
thanhhnk <thanhhnk@gmail.com>
2015-11-09 22:17:52 +0100

    Changed form's design

--- WindowsApp/JazzEventProject/JazzEventProject/Forms/ExitEvent.Designer.cs
+++ WindowsApp/JazzEventProject/JazzEventProject/Forms/ExitEvent.Designer.cs
@@ -295,7 +295,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(873, 410);
+            this.ClientSize = new System.Drawing.Size(884, 433);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox2);
