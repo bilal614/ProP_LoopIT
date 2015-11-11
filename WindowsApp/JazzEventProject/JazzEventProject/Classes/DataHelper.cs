6190f81034f453a34aaf9fe75d02a64c306978a9
BilalTButt <bilalbutt.614@gmail.com>
2015-11-11 13:22:34 +0100

    Changes not made

---
+++ WindowsApp/JazzEventProject/JazzEventProject/Classes/DataHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+namespace JazzEventProject.Classes
+{
+    class DataHelper
+    {
+        public MySqlConnection connection;
+
+        public DataHelper()
+        {
+            String connectionInfo = "server=athena01.fhict.local;" +
+                                    "database=dbi333151;" +
+                                    "user id=i333151;" +
+                                    "password=Chuotchit1504@" +
+                                    "connect timeout=30;";
+
+            connection = new MySqlConnection(connectionInfo);
+        }
+    }
+}
