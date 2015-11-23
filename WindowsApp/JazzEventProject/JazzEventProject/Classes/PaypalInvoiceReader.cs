4d104c042ffdb8e5cf20414fca1d6ce562d98af4
Michaberard <migelaens@gmail.com>
2015-11-23 16:34:38 +0100

    work on paypal

--- WindowsApp/JazzEventProject/JazzEventProject/Classes/PaypalInvoiceReader.cs
+++ WindowsApp/JazzEventProject/JazzEventProject/Classes/PaypalInvoiceReader.cs
@@ -3,11 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
 
 namespace JazzEventProject.Classes
 {
     class PaypalInvoiceReader
     {
-        int fact; 
+        // This class will read the data from the text file
+        public string tfile {get; set;} // name of the file
+ 
+        public PaypalInvoiceReader(string Tfile)
+        {
+            this.tfile = Tfile;
+        }
+
+        public List<string> readfile()
+        {
+            FileStream fs = null;
+            StreamReader sr = null;
+
+            List<string> b = new List<string>();
+
+            try
+            {
+                fs = new FileStream(tfile, FileMode.Open, FileAccess.Read);
+                sr = new StreamReader(fs);
+
+                string p = sr.ReadLine();
+                while (p != null)
+                {
+                    b.Add(p);
+                    int index = b.IndexOf(p);
+                    if (index > 3)
+                    {
+                        
+                        int[] arrID = new int[b.Count - 4];
+                        decimal[] arrB = new decimal[b.Count - 4];
+                        string[] arrS = p.Split(' ');
+                        foreach (string l in arrS)
+                        {
+                            arrS[0] = arrID[index - 4].ToString();
+                            arrS[1] = arrB[index - 4].ToString();
+                        }
+                        
+                    }
+                    p = sr.ReadLine();
+                }
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("Problem finding the file.");
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+                if (fs != null) fs.Close();
+            }
+
+            return b;
+        }
+
+
     }
 }
--- WindowsApp/JazzEventProject/JazzEventProject/Forms/PayPalTextFileTranslator.cs
+++ WindowsApp/JazzEventProject/JazzEventProject/Forms/PayPalTextFileTranslator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JazzEventProject.Classes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,28 @@ namespace JazzEventProject
         public PayPalTextFileTranslator()
         {
             InitializeComponent();
+        }
+
+        private PaypalInvoiceReader read;
+        public List<string> s;
+        private void btnLoadFileAndSave_Click(object sender, EventArgs e)
+        {
+            
+            // this is the loading part that will read the paypal text file. I used a predefined name for testing.
+            read = new PaypalInvoiceReader("test.txt");
+            s = read.readfile();
+            
+            foreach ( string m in s)
+            {
+               richTextBox1.Text += m + "\n";
+            }
+
+            // that part below is the saving part that will update the client balance
+
+            EventAccount currentAccount;
+            EventAccountDataHelper accountHelper = new EventAccountDataHelper();
+            accountHelper.UpdateAccountBalance()
+
         }
     }
 }
