a0248e78b176d6e91b8ce1bfe8167a511324e581
Michaberard <migelaens@gmail.com>
2016-01-04 15:06:16 +0100

    Website validation

--- WebsiteProject/PropWebsite/webPages/Contact_US.php
+++ WebsiteProject/PropWebsite/webPages/Contact_US.php
@@ -14,26 +14,55 @@
         <?php
             include('../includes/subPages/header.php');
         ?>
+        
         <div class="content">
         <div id="Regwrapper">
             <header class="regHeader">Please fill the form below to contact us!<span></span></header>
-            <form action="" method="post">
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
                 <div id ="Cont">
+                    <?php
+                    // define variables and set to empty values
+                    $nameErr = $emailErr = $messageErr = "";
+                    $name = $email = $message = "";
+
+                    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                        
+                                            
+                    if (empty($_POST["email"])) {
+                      $emailErr = " Please enter your Email.";
+                    } else {
+                      $email = test_input($_POST["email"]);
+                      // check if e-mail address is well-formed
+                      if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                        $emailErr = "Invalid email format";
+                      }
+                    }
+    
+                    }
+
+                        function test_input($data) {
+                           $data = trim($data);
+                           $data = stripslashes($data);
+                           $data = htmlspecialchars($data);
+                           return $data;
+                        }
+                        ?>
                                 <label>Name:</label><br/>
-                                <input type="text" name="name" required="required"  placeholder="Full Name"/><br/>
+                                <input type="text" name="name" required="required" placeholder="Full Name"/> *
+                                   <br><br>
                 
                                 <label>Email:</label><br/>
-                                <input type="text" name="name" required="required"  placeholder="Email address"/><br/>
+                                <input type="text" name="email" required="required" placeholder="Email address"/> 
+                                <span class="error"> * <?php echo $emailErr;?></span><br><br>                        
                             <label>Message:</label><br/>
-                            <textarea cols="50" rows="10" required="required" > </textarea>
-                            <br/>
+                            <textarea cols="50" rows="10" name="message" required></textarea>
+                              <br>                        
                             <input type="Submit" name="submit" value="Send"/>
                     </div>
                         </form>  
                         </div>
                    </div>
-                </form>
-    </div>
+               </div>
     <?php
         include('../includes/subPages/footer.php');
     ?>
