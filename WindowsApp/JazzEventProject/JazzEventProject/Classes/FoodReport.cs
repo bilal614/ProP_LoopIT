53e837d40887456ad0304c8165e5015891f88cb4
thanhhnk <thanhhnk@gmail.com>
2016-01-04 16:00:39 +0100

    Added FoodReport.cs

---
+++ WindowsApp/JazzEventProject/JazzEventProject/Classes/FoodReport.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JazzEventProject.Classes
+{
+    class FoodReport
+    {
+        public int FoodID { get; set; }
+        public string FoodName { get; set; }
+        public int AmountSold { get; set; }
+        public int AmountAvailable { get; set; }
+
+        public FoodReport(int foodID, string foodname, int sold, int available)
+        {
+            this.FoodName = foodname;
+            this.FoodID = foodID;
+            this.AmountSold = sold;
+            this.AmountAvailable = available;
+        }
+    }
+}
