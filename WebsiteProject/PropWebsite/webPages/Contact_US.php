4592f19812ecd06ab54a97f3493fdf29359d15ed
Michaberard <migelaens@gmail.com>
2016-01-11 00:48:36 +0100

    Add contact form validation

--- WebsiteProject/PropWebsite/webPages/Contact_US.php
+++ WebsiteProject/PropWebsite/webPages/Contact_US.php
@@ -1,11 +1,11 @@
-<?php include ('../includes/common.inc.php');?>
 <!DOCTYPE html>
 <html>
 <head>
-    <?php
+    <?php include ('../includes/common.inc.php');
         displayHeadTag();
     ?>
-   
+    <script src="../js/jquery.validate.min.js" type="text/javascript"></script>
+    <script src="../js/additional-methods.min.js" type="text/javascript"></script>
 </head>
 <body>
     <h1>
@@ -19,48 +19,22 @@
         <div class="content">
         <div id="Regwrapper">
             <header class="regHeader">Please fill the form below to contact us!<span></span></header>
-            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+            <?php if(isset($errors) && empty($errors) === false) { echo output_error($errors);  } ?>
+               <form action="mailto:michaberard@yahoo.fr" method="post" id="contactForm">
                 <div id ="Cont">
-                    <?php
-                    // define variables and set to empty values
-                    $nameErr = $emailErr = $messageErr = "";
-                    $name = $email = $message = "";
-
-                    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                        
-                                            
-                    if (empty($_POST["email"])) {
-                      $emailErr = " Please enter your Email.";
-                    } else {
-                      $email = test_input($_POST["email"]);
-                      // check if e-mail address is well-formed
-                      if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                        $emailErr = "Invalid email format";
-                      }
-                    }
-    
-                    }
-
-                        function test_input($data) {
-                           $data = trim($data);
-                           $data = stripslashes($data);
-                           $data = htmlspecialchars($data);
-                           return $data;
-                        }
-                        ?>
-                                <label>Name:</label><br/>
-                                <input type="text" name="name" required="required" placeholder=" Name"/> *
+                    <label for="name">Name:</label><br/>
+                                <input type="text" name="name" placeholder="Name"/> *
                                    <br><br>
                 
-                                <label>Email:</label><br/>
-                                <input type="text" name="email" required="required" placeholder="Email address"/> 
-                                <span class="error"> * <?php echo $emailErr;?></span><br><br>                        
-                            <label>Message:</label><br/>
-                            <textarea cols="50" rows="10" name="message" required></textarea>
+                                   <label for="email">Email:</label><br/>
+                                <input type="text" name="email" placeholder="Email address"/> 
+                                <span> * </span><br><br>                        
+                                <label for="message">Message:</label><br/>
+                            <textarea cols="50" rows="10" name="message"></textarea>
                               <br>                        
                             <input type="Submit" name="submit" value="Send"/>
                     </div>
-                        </form>  
+               </form>  
                         </div>
                    </div>
                </div>
